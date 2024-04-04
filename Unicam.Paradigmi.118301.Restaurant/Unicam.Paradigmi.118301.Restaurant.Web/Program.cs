350e5d0814eb4d16c99409ff3738c61b391a5c46
OmarEli92 <omar.elidrissi@studenti.unicam.it>
2024-04-04 19:06:31 +0200

    .

--- Unicam.Paradigmi.118301.Restaurant/Unicam.Paradigmi.118301.Restaurant.Web/Program.cs
+++ Unicam.Paradigmi.118301.Restaurant/Unicam.Paradigmi.118301.Restaurant.Web/Program.cs
@@ -12,22 +12,10 @@ builder.Services.AddApplicationService(builder.Configuration);
 builder.Services.AddWebService(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.AddWebMiddleware();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
